e2ab89a6bcb40457872c53a29bad6d4a1bb68b84
“sharanchauhan” <sharanchauhan29@gmail.com>
2022-05-13 21:10:26 +0530

    Added functionality to the ticket which gets saved on pressing Enter key

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-let addbtn=document.querySelector(".add-btn");
-let modal=document.querySelector(".modal-cont");
+let addbtn=document.querySelector(".add-btn")
+let modalCont=document.querySelector(".modal-cont")
+let mainCont = document.querySelector(".main-cont")
+let taskAreaCont=document.querySelector(".textarea-cont")
 let addModal=true;
 addbtn.addEventListener("click",function()
 {
     if(addModal)
     {
-        modal.style.display="flex";
+        modalCont.style.display="flex";
     }
     else
     {
-        modal.style.display="none";
+        modalCont.style.display="none";
     }
     addModal=!addModal;
 })
+
+modalCont.addEventListener("keydown",function(e){
+    if(e.key == 'Enter'){
+        createTicket(taskAreaCont.value);
+        taskAreaCont.value = "";
+        modalCont.style.display = "none";
+        addModal = !addModal;
+    }
+})
+
+function createTicket(task){
+    // <div class="ticket-cont">
+    //         <div class="ticket-color"></div>
+    //         <div class="ticket-id">#qzu03</div>
+    //         <div class="task-area">some task</div>
+    //     </div>
+    let ticketCont = document.createElement("div");
+    ticketCont.setAttribute('class','ticket-cont');
+    ticketCont.innerHTML = `<div class="ticket-color"></div>
+                            <div class="ticket-id">#qzu03</div>
+                            <div class="task-area">${task}</div>`
+    mainCont.appendChild(ticketCont);
+}
